58677387608447f562b9151c244e44e25546bf3b
weiguan <weiguanu@gmail.com>
2023-02-23 00:43:12 +0800

    weiguan: 1. init micrc-web compilation

--- micrc-bit/bit/compilations/micrc-web/commands/ConfigureCommand.ts
+++ micrc-bit/bit/compilations/micrc-web/commands/ConfigureCommand.ts
@@ -1,10 +1,12 @@
 /**
  * 配置命令
- * 执行intall, compile, link, status等命令配置workspace
+ * 执行install, compile, link, status等命令配置workspace
  * 下载(局部clone当前仓库)schema目录中的元数据文件, 放在node_modules/.cache/micrc/schema中
  * 读取schema中的context信息，判断当前是system还是domain，system仅允许atom, component类型组件和clientend客户端
  * domain仅允许module和state两类组件，检查schema中的目录，确定符合条件
  */
+import fs from 'fs';
+import { execSync } from 'child_process';
 import { Command, CommandOptions } from '@teambit/cli';
 
 export class ConfigureCmd implements Command {
@@ -23,10 +25,28 @@ export class ConfigureCmd implements Command {
   options = [] as CommandOptions;
 
   async report(): Promise<string> {
-    // 执行bit install && bit install && bit compile && bit compile && bit link && bit status命令并输出结果
+    // 执行bit compile && bit compile && bit link && bit status命令并输出结果
     // 使用git命令获取当前仓库地址，获取fetch的地址
     // 使用git浅克隆仓库的schema分支，放在本地node_modules/.cache/micrc-schema中
     // 使用git checkout下载schema目录
+    try {
+      execSync(
+        'bit compile && bit compile && bit link',
+        {
+          encoding: 'utf8',
+        },
+      );
+    } catch (e) {
+      return Promise.resolve(`${this.name} bit error`);
+    }
+    try {
+      execSync('git -v');
+      if (!fs.existsSync('./node_modules/.cache/micrc/schema')) {
+        return Promise.resolve(`${this.name} git clone error`);
+      }
+    } catch (e) {
+      return Promise.resolve(`${this.name} git error`);
+    }
     return Promise.resolve(`${this.name} complete`);
   }
 }
--- micrc-bit/bit/compilations/micrc-web/commands/GenerateCommand.ts
+++ micrc-bit/bit/compilations/micrc-web/commands/GenerateCommand.ts
@@ -4,6 +4,7 @@
  * 循环使用bit generator命令，使用不同的模版，通过元数据创建组件。若组件已经存在，则删除重新创建
  * 这里并不处理版本，pub命令处理tag和version问题，并被ci使用
  */
+import fs from 'fs';
 import { Command, CommandOptions } from '@teambit/cli';
 
 export class GenerateCmd implements Command {
@@ -21,13 +22,28 @@ export class GenerateCmd implements Command {
 
   options = [] as CommandOptions;
 
-  async report(): Promise<string> {
+  designSystemGen = (): Promise<string> => {
+    // todo 处理atom组件
+    // todo 处理component组件
+    // 读取components目录下的所有文件并循环处理
+    // fs.readdirSync('').
+    // todo 处理端口
     // 查找atoms目录下所有文件，文件名为组件id，通过id截取组件name，调用bit create micrc-web-atom name创建组件
     // 同上创建components目录下的组件
     // 同上创建states目录下的状态组件
     // 同上创建modules目录下的组件
     // 同上创建clientend目录下的app组件
     // 输出创建的结果，涉及哪些组件，哪些是新建的，哪些是重建（修改）的
-    return Promise.resolve(`${this.name} complete`);
+  };
+
+  domainDesignGen = (): Promise<string> => {};
+
+  async report(): Promise<string> {
+    // todo 读取schema下的context-info.json文件，判断isDesignSystem
+    const isDesignSystem = true;
+    if (isDesignSystem) {
+      return this.designSystemGen();
+    }
+    return this.domainDesignGen();
   }
 }
