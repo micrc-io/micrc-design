ecddd0e91b6279616c3cd3e4695e0546d1c8eaa8
weiguan <weiguanu@gmail.com>
2023-02-27 04:03:26 +0800

    weiguan: 1. complete configure in micrc-web compilation

--- micrc-bit/bit/compilations/micrc-web/commands/ConfigureCommand.ts
+++ micrc-bit/bit/compilations/micrc-web/commands/ConfigureCommand.ts
@@ -5,13 +5,85 @@
  * 读取schema中的context信息，判断当前是system还是domain，system仅允许atom, component类型组件和clientend客户端
  * domain仅允许module和state两类组件，检查schema中的目录，确定符合条件
  */
-import fs from 'fs';
-import { execSync } from 'child_process';
+import path from 'path';
+
 import { Command, CommandOptions } from '@teambit/cli';
+
 import chalk from 'chalk';
 import log from 'loglevel';
 
+import { execCmd, invoke } from '../lib/process';
+
 log.setLevel('INFO');
+
+// 合并schema分支, 检出最新当前分支
+const checkout = async () => {
+  const gitBasePath = path.resolve(
+    require.resolve('@micrc/bit.compilations.micrc-web'),
+    '../../../../', // node_modules目录,
+    '../', // bit workspace根目录
+    '../', // git根目录
+  );
+  // 获取当前分支名
+  const branch: string = await invoke('git rev-parse --abbrev-ref HEAD', gitBasePath);
+  try {
+    // 切换到schema分支
+    await execCmd('git', ['checkout', 'schema'], gitBasePath);
+    // 从服务器拉取最新内容
+    await execCmd('git', ['fetch'], gitBasePath);
+    // 合并最新内容到schema
+    await execCmd('git', ['merge'], gitBasePath);
+    // 切换回当前分支, 并merge schema分支
+    await execCmd('git', ['checkout', branch.replace('\n', '')], gitBasePath);
+    await execCmd('git', ['merge', 'schema'], gitBasePath);
+    log.info(chalk.green('schema merged successfully.'));
+  } catch (e) {
+    log.error(chalk.red(`schema merge error: ${e}`));
+  }
+};
+
+const copy = async () => {
+  const nodeModulesBasePath = path.resolve(
+    require.resolve('@micrc/bit.compilations.micrc-web'),
+    '../../../../', // node_modules目录,
+  );
+  const gitBasePath = path.resolve(
+    nodeModulesBasePath,
+    '../', // bit workspace根目录
+    '../', // git根目录
+  );
+  // 首先删除.cache/micrc/schema目录
+  const schemaSourcePath = path.resolve(gitBasePath, 'schema');
+  const schemaTargetPath = path.resolve(nodeModulesBasePath, '.cache', 'micrc', 'schema');
+  try {
+    await execCmd('rm', ['-rf', schemaTargetPath], gitBasePath);
+    // 创建目录并拷贝
+    await execCmd('mkdir', ['-p', schemaTargetPath], gitBasePath);
+    await execCmd('cp', ['-r', schemaSourcePath, schemaTargetPath], gitBasePath);
+    log.info(chalk.green('schema copied successfully.'));
+  } catch (e) {
+    log.error(chalk.red(`schema copy error: ${e}`));
+  }
+};
+
+const init = async () => {
+  const bitBasePath = path.resolve(
+    require.resolve('@micrc/bit.compilations.micrc-web'),
+    '../../../../', // node_modules目录,
+    '../', // bit workspace根目录
+  );
+  try {
+    await execCmd('bit', ['install'], bitBasePath);
+    await execCmd('bit', ['install'], bitBasePath);
+    await execCmd('bit', ['compile'], bitBasePath);
+    await execCmd('bit', ['compile'], bitBasePath);
+    await execCmd('bit', ['link'], bitBasePath);
+    await execCmd('bit', ['status'], bitBasePath);
+    log.info(chalk.green('workspace initialized successfully.'));
+  } catch (e) {
+    log.error(chalk.red(`workspace init error: ${e}`));
+  }
+};
 
 export class ConfigureCmd implements Command {
   name = 'micrc:conf';
@@ -29,30 +101,10 @@ export class ConfigureCmd implements Command {
   options = [] as CommandOptions;
 
   async report(): Promise<string> {
-    log.info();
-    log.info(chalk.yellow('test'));
-    // 执行bit compile && bit compile && bit link && bit status命令并输出结果
-    // 使用git命令获取当前仓库地址，获取fetch的地址
-    // 使用git浅克隆仓库的schema分支，放在本地node_modules/.cache/micrc-schema中
-    // 使用git checkout下载schema目录
-    try {
-      execSync(
-        'bit compile && bit compile && bit link',
-        {
-          encoding: 'utf8',
-        },
-      );
-    } catch (e) {
-      return Promise.resolve(`${this.name} bit error`);
-    }
-    try {
-      execSync('git -v');
-      if (!fs.existsSync('./node_modules/.cache/micrc/schema')) {
-        return Promise.resolve(`${this.name} git clone error`);
-      }
-    } catch (e) {
-      return Promise.resolve(`${this.name} git error`);
-    }
+    log.info('');
+    await init();
+    await checkout();
+    await copy();
     return Promise.resolve(`${this.name} complete`);
   }
 }
