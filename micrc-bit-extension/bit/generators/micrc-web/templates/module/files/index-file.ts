3dd13f991db7df4a74a95e65fe9398080278d995
wuwanping <wanpingwua@gmail.com>
2023-03-29 16:05:30 +0800

    wpwu: update bug

--- micrc-bit-extension/bit/generators/micrc-web/templates/module/files/index-file.ts
+++ micrc-bit-extension/bit/generators/micrc-web/templates/module/files/index-file.ts
@@ -6,5 +6,6 @@ import { ModuleContextData } from '../_parse';
 export function indexFile(data: ModuleContextData) {
   return `export { ${data.context.namePascalCase} } from './${data.context.name}';
 export type { ${data.context.namePascalCase}Props } from './${data.context.name}';
+require('axios');
 `;
 }
