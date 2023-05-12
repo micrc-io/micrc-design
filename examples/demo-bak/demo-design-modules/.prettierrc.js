2ef2a7cb352e2a258fbec503ee80297d5328a297
wuwanping <wanpingwua@gmail.com>
2023-05-12 22:57:31 +0800

    修改模块元数据，端口代理

---
+++ examples/demo-bak/demo-design-modules/.prettierrc.js
@@ -0,0 +1,7 @@
+// noinspection NpmUsedModulesInstalled,JSUnresolvedFunction
+const baseConfig = require("@teambit/react/prettier/prettier.config");
+
+// noinspection JSUnresolvedVariable
+module.exports = {
+  ...baseConfig,
+};
