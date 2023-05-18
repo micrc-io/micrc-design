07533c3572bf63f0a244761b9d17d39fde6e3856
wuwanping <wanpingwua@gmail.com>
2023-05-19 00:49:50 +0800

    更新端口favicon

--- micrc-bit-extension/bit/generators/micrc-web/templates/clientend/files/app/public/favicon-file.ts
+++ micrc-bit-extension/bit/generators/micrc-web/templates/clientend/files/app/public/favicon-file.ts
@@ -1,8 +1,6 @@
 /**
  * app/public/favicon-file
  */
-import fs from 'fs';
-import path from 'path';
 import request from 'sync-request';
 
 import chalk from 'chalk';
@@ -12,17 +10,11 @@ import { ClientendContextData } from '../../../_parser';
 
 log.setLevel('INFO');
 
-const PUBLIC_PATH = ['app', 'public'];
-
 export function faviconFile(data: ClientendContextData) {
-  const publicPath = path.resolve(data.intro.sourceDir, ...PUBLIC_PATH);
-  if (!fs.existsSync(publicPath)) {
-    fs.mkdirSync(publicPath, { recursive: true });
-  }
   try {
-    fs.writeFileSync(path.join(publicPath, 'favicon.ico'), request('GET', data.intro.favicon).body);
+    return request('GET', data.intro.favicon).body;
   } catch (e) {
     log.error(chalk.red(`download image error: ${e}`));
+    return '';
   }
-  return '';
 }
