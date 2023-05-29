17f5dc7f9c13acfb98b2f09e15def6b40f03e741
weiguan <weiguanu@gmail.com>
2023-05-29 20:39:24 +0800

    weiguan: 1. page of "/" redirected to first page on first layout

--- micrc-bit-extension/bit/generators/micrc-web/templates/clientend/files/app/entry-file.ts
+++ micrc-bit-extension/bit/generators/micrc-web/templates/clientend/files/app/entry-file.ts
@@ -9,9 +9,10 @@ import { propsAssembler, jsonObject } from '../../../../lib/assembler';
 import type { ClientendContextData } from '../../_parser';
 
 const tmpl = `// 入口文件, 处理端口布局、global store初始化(i18n、tracker、integration)
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect } from 'react';
 import App from 'next/app';
 import type { AppProps } from 'next/app';
+import type { NextRouter } from 'next/router';
 import { useRouter } from 'next/router';
 import { MDXProvider } from '@mdx-js/react';
 
@@ -36,6 +37,13 @@ import integration from '../meta/integration.json';
 
 const permissions: Record<string, Array<string>> = permission;
 
+const env = process.env.NEXT_PUBLIC_APP_ENV || process.env.APP_ENV;
+let locale = 'en_US';
+if (env && (env === 'default' || env === 'local')) {
+  locale = 'zh_CN'; // todo 获取开发机系统语言
+}
+initGlobalStore(locale, i18n, i18ns, tracker, integration);
+
 const layouts: Record<string, {uris: Array<string>; layout: (props: any) => ReactNode }> = {
   {{#each layouts}}
   {{@key}}: { uris: {{{json this.uris}}}, layout:(props) => <{{@key}} {{{propsAssembler this.props}}} /> },
@@ -53,21 +61,37 @@ const Wrapper = (props: JSX.IntrinsicAttributes) => {
   if (!Layout) {
     throw Error(\`unhandled clientends layout for page: \${router.pathname}\`);
   }
-  const env = process.env.NEXT_PUBLIC_APP_ENV || process.env.APP_ENV;
-  let locale = 'en_US';
-  if (env && (env === 'default' || env === 'local')) {
-    locale = 'zh_CN'; // todo 获取开发机系统语言
-  }
-  initGlobalStore(locale, i18n, i18ns, tracker, integration);
   return React.cloneElement(Layout, { ...props });
 };
 
-const authcRouter = (router: any) => {
+const redirectAuthcRouter = async (router: NextRouter) => {
   if (typeof window !== 'undefined') {
+    const redirectUri = router.pathname;
+    const appEnv = process.env.NEXT_PUBLIC_APP_ENV;
+    if (redirectUri === '/') {
+      const layoutData = Object.values(layouts);
+      if (layoutData.length <= 0) {
+        throw Error('at least one layout for redirect from "/". ');
+      }
+      const uris = layoutData[0].uris;
+      if (uris.length <= 0) {
+        throw Error('at least one page for redirect from "/". ');
+      }
+      if (appEnv === 'default' || appEnv === 'local') {
+        window.location.replace(uris[0]);
+      } else {
+        await router.push(uris[0]);
+      }
+      return;
+    }
     const global: any = useGlobalStore.getState();
     const loginUri = process.env.NEXT_PUBLIC_LOGIN_PAGE_URI || '/security/authc';
-    if (!global.subject.id && router && router.pathname !== loginUri) {
-      window.location.replace(loginUri);
+    if (!global.subject.id && router && redirectUri !== loginUri) {
+      if (appEnv === 'default' || appEnv === 'local') {
+        window.location.replace(\`\${loginUri}?redirectUri=\${redirectUri}\`);
+      } else {
+        await router.push(\`\${loginUri}?redirectUri=\${redirectUri}\`);
+      }
     }
   }
 };
@@ -75,7 +99,13 @@ const authcRouter = (router: any) => {
 export default function MicrcApp({ Component, pageProps }: AppProps) {
   const components = { wrapper: Wrapper };
   const router = useRouter();
-  authcRouter(router);
+  useEffect(() => {
+    const redirect = async () => {
+      await redirectAuthcRouter(router);
+    };
+    redirect();
+  });
+
   return (
     <MDXProvider components={ components }>
       <Authorized
